03e9828b14ed5617d90181066a801ba46561ef22
SELVAPRASANTH-SELLASAMY <prasanthsamy61@gmail.com>
2023-09-04 00:22:10 +0530

    Operational optimization

--- src/menu.js
+++ src/menu.js
@@ -11,14 +11,73 @@ import LeftMenu from './Leftelement';
 import ProfileMenu from './ProfileMenu';
 import Notify from './Notification';
 import Create from './Create';
+import Video from './video';
+import Suggesstions from './Suggesstions';
 function Menu()
 {
     const [MenuBar, showMenu] = useState(false);
     const [profileBar, ShowProfilebar] = useState(false);
     const [notificationBar,ShowNotificationBar]=useState(false);
     const [createBar,ShowCreateBar]=useState(false);
+    const [clicked,setClickStatus]=useState(false);
+    const anime={
+        animationName:clicked?'alexa':null,
+        animationDuration:'1s',
+        animationIterationCount:5,
+        animationTimingFunction:'linear'
+    }
+
+
+    const channels=[
+        'Tutor Joe\'s',
+        'Kalvium',
+        'A2D Tech',
+        'Google',
+        'Snapdragon india',
+        'Funtouch OS',
+        'Oh my thagaval',
+        'Pudhumai sei',
+        'Tamil trekker',
+        'Tech boss',
+        'Tamil tech',
+        'Tutor Joe\'s',
+        'Kalvium',
+        'A2D Tech',
+        'Google',
+        'Snapdragon india',
+        'Funtouch OS',
+        'Oh my thagaval',
+        'Pudhumai sei',
+        'Tamil trekker',
+        'Tech boss',
+        'Tamil tech',
+        'Tutor Joe\'s',
+        'Kalvium',
+        'A2D Tech',
+        'Google',
+        'Snapdragon india',
+        'Funtouch OS',
+        'Oh my thagaval',
+        'Pudhumai sei',
+        'Tamil trekker',
+        'Tech boss',
+        'Tamil tech',
+        'Tutor Joe\'s',
+        'Kalvium',
+        'A2D Tech',
+        'Google',
+        'Snapdragon india',
+        'Funtouch OS',
+        'Oh my thagaval',
+        'Pudhumai sei',
+        'Tamil trekker',
+        'Tech boss',
+        'Tech boss',
+        'Tamil tech'
+    ];
+
     return(
-        <>
+        <div className="Menu-cont">
         <div className="top-menu">
             <div className="menu">
                     <div onClick={()=>showMenu(!MenuBar)} className="hamburger-menu">
@@ -37,14 +96,14 @@ function Menu()
                         <img src={search} alt="search-icon" />
                         <p className="what-is-it">Search</p>
                     </div>
-                    <div className="microphone">
+                    <div onMouseDown={()=>setClickStatus(false)} onClick={()=>setClickStatus(true)} className="microphone" style={anime}>
                         <img src={mic} alt="voice-search" />
                         <p className="what-is-it">Search with your voice</p>
                     </div>
                 </div>
                 <div className="right-corner-menu">
                         <div className="control-create">
-                            <div onClick={()=>ShowCreateBar(!createBar)} className="create">
+                            <div onClick={()=>{ShowCreateBar(!createBar);ShowNotificationBar(false);ShowProfilebar(false)}} className="create">
                                 <img src={create} alt="create" />
                                 {createBar?null:<p className="what-is-it">Create</p>}
                             </div>
@@ -53,7 +112,7 @@ function Menu()
                             </div>
                         </div>
                         <div className="controlNotification">
-                            <div onClick={()=>ShowNotificationBar(!notificationBar)} className="notify">
+                            <div onClick={()=>{ShowNotificationBar(!notificationBar);ShowCreateBar(false);ShowProfilebar(false)}} className="notify">
                                 <p className="notification-count">1</p>
                                 <img src={notify} alt="notifications" />
                                 {notificationBar?null:<p className="what-is-it">Notifications</p>}
@@ -62,7 +121,7 @@ function Menu()
                                 {notificationBar?<Notify/>:null}
                             </div>
                         </div>
-                        <div onClick={() => ShowProfilebar(!profileBar)} className="profile">
+                        <div onClick={() => {ShowProfilebar(!profileBar);ShowCreateBar(false);ShowNotificationBar(false)}} className="profile">
                             <img src={profile} alt="profile" />
                         </div>
                         {profileBar ? <ProfileMenu /> : null}
@@ -72,7 +131,17 @@ function Menu()
             {
                 MenuBar ? <LeftMenu /> : null
             }
-        </>
+            <div className="suggesstion-area">
+                <Suggesstions/>
+            </div>
+            <div className="video-display-area">
+                {
+                    channels.map((ch)=>(
+                        <Video/>
+                    ))
+                }
+            </div>
+        </div>
     );
 }
 export default Menu;
